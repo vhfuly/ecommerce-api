91528e5424facac40308f6b983d0b83617ec24ee
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-10 20:23:56 -0300

    feat: add eslint

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 import cors from 'cors';
-import {ValidationError} from 'express-validation';
-
+import { ValidationError } from 'express-validation';
 import { router } from './routes/index';
 import { dbs } from './config/database';
 
@@ -15,32 +14,29 @@ app.use(express.json());
 const isProduction = process.env.NODE_ENV === 'production';
 const PORT = process.env.PORT || 3000;
 
-app.use('/public', express.static(__dirname + '/public'));
-app.use('/public/images', express.static(__dirname + '/public/images'));
-
-
+app.use('/public', express.static(`${__dirname}src/public`));
+app.use('/public/images', express.static(`${__dirname}src/public/images`));
 
 const dbURI = isProduction ? dbs.dbProduction : dbs.dbTest;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true  })
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('connection successful'))
-  .catch((err) => console.error(err));;
+  .catch((err) => console.error(err));
 
+app.set('view engine', 'ejs');
 
-app.set('view engine', 'ejs')
-
-if(!isProduction) app.use(morgan('dev'));
+if (!isProduction) app.use(morgan('dev'));
 app.use(cors());
 app.disable('x-powered-by');
 app.use(compression());
 
-app.use(bodyParser.urlencoded({ extended: false, limit: 1.5*1024*1024 }));
-app.use(bodyParser.json({ limit: 1.5*1024*1024 }));
+app.use(bodyParser.urlencoded({ extended: false, limit: 1.5 * 1024 * 1024 }));
+app.use(bodyParser.json({ limit: 1.5 * 1024 * 1024 }));
 
-app.use("/", router);
+app.use('/', router);
 
-app.use((err: ValidationError , request:Request, response: Response, _next: NextFunction) => {
+app.use((err: ValidationError, request:Request, response: Response) => {
   response.status(err.statusCode || 500);
-  if(err.statusCode !== 404) console.warn("Error: ", err, new Date());
+  if (err.statusCode !== 404) console.warn('Error: ', err, new Date());
   response.json(err);
 });
 
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,154 +1,159 @@
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response, NextFunction } from 'express';
 
 import User, { UserDocument } from '../models/User';
 import sendEmailRecovery from '../helpers/emailRecovery';
 
-import { setPassword, sendAuthJSON, validatePassword, createTokenRecoveryPassword, finishTokenRecoveryPassword } from '../utils/password';
+import {
+  setPassword,
+  sendAuthJSON,
+  validatePassword,
+  createTokenRecoveryPassword,
+  finishTokenRecoveryPassword,
+} from '../utils/password';
 
 class UserController {
-  async index(request: Request, response: Response , next: NextFunction) {
+  async index(request: Request, response: Response): Promise<any> {
     try {
-      const user: UserDocument = await User.findById(request.headers.id)
-      if(!user) return response.status(401).json({ error: "Unregistered user" });
-      return response.json({ user: sendAuthJSON(user) })
+      const user: UserDocument = await User.findById(request.headers.id);
+      if (!user) return response.status(401).json({ error: 'Unregistered user' });
+      return response.json({ user: sendAuthJSON(user) });
     } catch (error) {
-      response.json({error : error});
+      response.json({ error });
     }
   }
 
-  async show(request: Request, response: Response , next: NextFunction) {
+  async show(request: Request, response: Response, next: NextFunction) {
     try {
       const { id } = request.params;
-      const user: UserDocument = await User.findOne({_id: id});
+      const user: UserDocument = await User.findOne({ _id: id });
       // .populate({ path: 'store'})
-      if(!user) return response.status(401).json({ error: "Unregistered user" });
+      if (!user) return response.status(401).json({ error: 'Unregistered user' });
       return response.json({
         user: {
           name: user.name,
           email: user.email,
           permission: user.permission,
           store: user.store,
-        }
-      })
+        },
+      });
     } catch (error) {
-      response.status(500).json({error :'Error to find the user'});
+      response.status(500).json({ error: 'Error to find the user' });
     }
   }
 
-
-  async store(request: Request, response: Response , next: NextFunction) {
+  async store(request: Request, response: Response, next: NextFunction) {
     try {
-      const {name, email, password, store } = request.body
-      const { salt, hash }  = setPassword(password)
-      const account = await User.findOne({email})
-      if(account) return response.status(422).json({ error: 'The email already has an account'})
-      const user: UserDocument = new User({ name , email, salt, hash, store });
-      response.json({ user: sendAuthJSON(user)})
-      await user.save()
+      const {
+        name, email, password, store,
+      } = request.body;
+      const { salt, hash } = setPassword(password);
+      const account = await User.findOne({ email });
+      if (account) return response.status(422).json({ error: 'The email already has an account' });
+      const user: UserDocument = new User({
+        name, email, salt, hash, store,
+      });
+      response.json({ user: sendAuthJSON(user) });
+      await user.save();
     } catch (error) {
-      response.status(500).json({error :'Error to save user'});
+      response.status(500).json({ error: 'Error to save user' });
     }
   }
 
-  async update(request: Request, response: Response , next: NextFunction) {
+  async update(request: Request, response: Response, next: NextFunction) {
     try {
-      const {name, email, password } = request.body
-      const user: UserDocument = await User.findById(request.headers.id)
-      if(!user) return response.status(401).json({ error: "Unregistered user" });
+      const { name, email, password } = request.body;
+      const user: UserDocument = await User.findById(request.headers.id);
+      if (!user) return response.status(401).json({ error: 'Unregistered user' });
       if (typeof name !== 'undefined') user.name = name;
       if (typeof email !== 'undefined') user.email = email;
       if (typeof password !== 'undefined') {
-        const { salt, hash }  = setPassword(password);
+        const { salt, hash } = setPassword(password);
         user.salt = salt;
         user.hash = hash;
       }
-      console.log(user)
-      await user.save()
-      response.json({ user: sendAuthJSON(user)})
+      console.log(user);
+      await user.save();
+      response.json({ user: sendAuthJSON(user) });
     } catch (error) {
-      response.status(500).json({error :'Error to update user'});
+      response.status(500).json({ error: 'Error to update user' });
     }
   }
 
-  async remove(request: Request, response: Response , next: NextFunction) {
+  async remove(request: Request, response: Response, next: NextFunction) {
     try {
-      const user: UserDocument = await User.findById(request.headers.id)
-      if(!user) return response.status(401).json({ error: "Unregistered user" });
-      await user.remove()
+      const user: UserDocument = await User.findById(request.headers.id);
+      if (!user) return response.status(401).json({ error: 'Unregistered user' });
+      await user.remove();
       return response.json({ deleted: true });
     } catch (error) {
-      response.status(500).json({error :'Error to remove user'});
+      response.status(500).json({ error: 'Error to remove user' });
     }
   }
 
-  async login(request: Request, response: Response , next: NextFunction) {
+  async login(request: Request, response: Response, next: NextFunction) {
     try {
-      const { email, password } =request.body;
-      const user: UserDocument = await User.findOne({ email })
-      if(!user) return response.status(401).json({ error: "Unregistered user" });
-      if(!validatePassword(password, user)) return response.status(401).json({ error: 'invalid password' });
+      const { email, password } = request.body;
+      const user: UserDocument = await User.findOne({ email });
+      if (!user) return response.status(401).json({ error: 'Unregistered user' });
+      if (!validatePassword(password, user)) return response.status(401).json({ error: 'invalid password' });
       return response.json({ user: sendAuthJSON(user) });
     } catch (error) {
-      response.status(500).json({error :'Login error'});
+      response.status(500).json({ error: 'Login error' });
     }
   }
 
   // RECOVERY
 
-  showRecovery(request: Request, response: Response , next: NextFunction) {
+  showRecovery(request: Request, response: Response, next: NextFunction) {
     return response.render('recovery', { error: null, success: null });
   }
 
-
-  async createRecovery(request: Request, response: Response , next: NextFunction) {
+  async createRecovery(request: Request, response: Response, next: NextFunction) {
     try {
       const { email } = request.body;
-      if(!email) return response.render('src/recovery', { error: 'Fill in with your email', success: null });
+      if (!email) return response.render('src/recovery', { error: 'Fill in with your email', success: null });
       const user: UserDocument = await User.findOne({ email });
-      if(!user) return response.render("recovery", { error: 'There is no user with this email', success: null });
+      if (!user) return response.render('recovery', { error: 'There is no user with this email', success: null });
       const recoveryData = createTokenRecoveryPassword(user);
-      sendEmailRecovery({ user, recovery: recoveryData }, (error = null, success = null) => {
-        return response.render('recovery', { error, success });
-      });
+      sendEmailRecovery({ user, recovery: recoveryData }, (error = null, success = null) => response.render('recovery', { error, success }));
     } catch (error) {
-      response.status(500).json({error :'Error in sending email'});
+      response.status(500).json({ error: 'Error in sending email' });
     }
   }
 
-  async showCompleteRecovery(request: Request, response: Response , next: NextFunction) {
+  async showCompleteRecovery(request: Request, response: Response, next: NextFunction) {
     try {
-      if(!request.query.token) return response.render('recovery', { error: 'Unidentified token', success: null });
+      if (!request.query.token) return response.render('recovery', { error: 'Unidentified token', success: null });
       const user: UserDocument = await User.findOne({ 'recovery.token': request.query.token });
-      if(!user) return response.render('recovery', { error: 'There is no user with this token', success: null });
-      if( new Date(user.recovery.date) < new Date() ) return response.render('recovery', { error: 'Expired token. Try again.', success: null });
+      if (!user) return response.render('recovery', { error: 'There is no user with this token', success: null });
+      if (new Date(user.recovery.date) < new Date()) return response.render('recovery', { error: 'Expired token. Try again.', success: null });
       return response.render('recovery/store', { error: null, success: null, token: request.query.token });
     } catch (error) {
-      response.status(500).json({error :'Error to save new password'});
+      response.status(500).json({ error: 'Error to save new password' });
     }
   }
 
-  async completeRecovery(request: Request, response: Response , next: NextFunction) {
+  async completeRecovery(request: Request, response: Response, next: NextFunction) {
     try {
       const { token, password } = request.body;
-      if(!token || !password) return response.render('recovery/store', { error: 'Please fill in again with your new password', success: null, token: token });
+      if (!token || !password) return response.render('recovery/store', { error: 'Please fill in again with your new password', success: null, token });
       const user: UserDocument = await User.findOne({ 'recovery.token': token });
-      if(!user) return response.render('recovery', { error: 'Unidentified user', success: null });
+      if (!user) return response.render('recovery', { error: 'Unidentified user', success: null });
 
       finishTokenRecoveryPassword(user);
-      const { salt, hash }  = setPassword(password);
+      const { salt, hash } = setPassword(password);
       user.salt = salt;
       user.hash = hash;
-      await user.save()
+      await user.save();
       return response.render('recovery/store', {
-          error: null,
-          success: 'Password changed successfully. Try logging in again...',
-          token: null
+        error: null,
+        success: 'Password changed successfully. Try logging in again...',
+        token: null,
       });
     } catch (error) {
-      response.status(500).json({error :'Error to save new password'});
+      response.status(500).json({ error: 'Error to save new password' });
     }
   }
-
 }
 
 export { UserController };
