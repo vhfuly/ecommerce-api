eb74a378435702301541101d91117f7830eebfe7
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-11 18:20:49 -0300

    feat: create clientsRouter

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { usersRouter } from './Users';
-import { storesRouter } from './Store';
+import { storesRouter } from './Stores';
+import { clientsRouter } from './Clients';
 
 const router = Router();
 
 router.use('/users', usersRouter)
 router.use('/stores', storesRouter)
+router.use('/clients', clientsRouter)
 
 export { router as routerApi }
