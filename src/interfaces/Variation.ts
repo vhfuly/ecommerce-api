6555d80fee166d26b9f8132a295caa9d2667b388
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-14 17:16:50 -0300

    feat: create VariationInterface

---
+++ src/interfaces/Variation.ts
@@ -0,0 +1,22 @@
+import { Types } from 'mongoose';
+
+
+export interface VariationInterface {
+  code: string;
+  name: string;
+  price: string;
+  photos: Array<string>;
+  sale: number;
+  delivery: {
+    dimensions: {
+      heightCm: number;
+      widthCm: number;
+      depthCm: number;
+    },
+    weightKg: number;
+    freeShipping: boolean;
+  }
+  amount: number;
+  product: Types.ObjectId;
+  store: Types.ObjectId;
+}
