0e630d78277479ad8946e32d8f9f093b5200f844
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-14 07:53:57 -0300

    feat: createproductValidation

---
+++ src/controllers/validations/ProductValidation.ts
@@ -0,0 +1,80 @@
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+
+const productValidation = { 
+  store: {
+    body: Joi.object().keys({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      category: Joi.string().alphanum().length(24).required(),
+      price: Joi.number().required(),
+      sale: Joi.number(),
+      sku: Joi.string().required(), 
+    }),
+  },
+
+  update: {
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24).required(),
+    }),
+    body: Joi.object().keys({
+      title: Joi.string().optional(),
+      description: Joi.string().optional(),
+      category: Joi.string().alphanum().length(24).optional(),
+      price: Joi.number().optional(),
+      sale: Joi.number(),
+      sku: Joi.string().optional(),
+      availability: Joi.boolean().optional(),
+    }),
+  },
+
+  updateImages: {
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24).required(),
+    }),
+  },
+
+  remove: {
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24).required(),
+    }),
+  },
+
+  index: {
+    query: Joi.object().keys({
+      store: Joi.string().alphanum().length(24).required(),
+      limit: Joi.number(),
+      offset: Joi.number(),
+      sortType: Joi.string(),
+    }),
+  },
+
+  indexAvailable: {
+    query: Joi.object().keys({
+      store: Joi.string().alphanum().length(24).required(),
+      limit: Joi.number(),
+      offset: Joi.number(),
+      sortType: Joi.string(),
+    }),
+  },
+
+  search: {
+    params: Joi.object().keys({
+      search: Joi.string().required(),
+    }),
+    query: Joi.object().keys({
+      store: Joi.string().alphanum().length(24).required(),
+      limit: Joi.number(),
+      offset: Joi.number(),
+      sortType: Joi.string(),
+    }),
+  },
+
+  show: {
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24).required(),
+    }),
+  },
+};
+
+export { productValidation };
--- src/routes/api/v1/Products.ts
+++ src/routes/api/v1/Products.ts
@@ -4,7 +4,7 @@ import { validate } from 'express-validation';
 import { auth } from '../../auth';
 import { upload } from '@config/multer';
 import { storeValidation } from '@validations/StoreValidation';
-import { clientValidation } from '@validations/ClientValidation';
+import { productValidation } from '@validations/ProductValidation';
 import { ProductController } from '@controllers/ProductController';
 
 const productController = new ProductController();
@@ -12,16 +12,20 @@ const productController = new ProductController();
 const router = Router();
 
 // ADMIN
-router.post("/", auth.required, storeValidation.admin, productController.store);
-router.put("/:id", auth.required, storeValidation.admin, productController.update);
-router.put("/images/:id", auth.required, storeValidation.admin, upload.array("files", 4), productController.updateImages);
-router.delete("/:id", auth.required, storeValidation.admin, productController.remove);
+router.post("/", auth.required, storeValidation.admin,
+  validate(productValidation.store), productController.store);
+router.put("/:id", auth.required, storeValidation.admin,
+  validate(productValidation.update), productController.update);
+router.put("/images/:id", auth.required, storeValidation.admin, validate(productValidation.updateImages),
+  upload.array("files", 4), productController.updateImages);
+router.delete("/:id", auth.required, storeValidation.admin,
+  validate(productValidation.remove), productController.remove);
 
 // CLIENTS
-router.get("/", productController.index);
-router.get("/available", productController.indexAvailable);
-router.get("/search/:search", productController.search);
-router.get("/:id", productController.show);
+router.get("/", validate(productValidation.index), productController.index);
+router.get("/available", validate(productValidation.indexAvailable), productController.indexAvailable);
+router.get("/search/:search", validate(productValidation.search), productController.search);
+router.get("/:id", validate(productValidation.show), productController.show);
 
 // VARIATIONS
 
