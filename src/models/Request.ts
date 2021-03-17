7f261d75d73dc670f686e62d2888c3c8ed256207
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-17 18:25:13 -0300

    feat: create RequestModel

--- src/models/Request.ts
+++ src/models/Request.ts
@@ -4,12 +4,19 @@ export interface RequestDocument extends RequestInterface, Document {}
 
 const RequestSchema: Schema<RequestDocument, RequestModel> = new Schema({
   client: {type: Schema.Types.ObjectId, ref:'Client', required: true },
-  cart: [{
-    variation: {type: Schema.Types.ObjectId, ref:'Variation', required: true },
-    product: {type: Schema.Types.ObjectId, ref:'Product', required: true  },
-    amount: { type: Number, default: 1 },
-    unitPrice: { type: Number, required: true },
-  }]
+  cart: {
+    type: [{
+      variation: {type: Schema.Types.ObjectId, ref:'Variation', required: true },
+      product: {type: Schema.Types.ObjectId, ref:'Product', required: true  },
+      staticProduct: { type: String },
+      amount: { type: Number, default: 1 },
+      unitPrice: { type: Number, required: true },
+    }]
+  },
+  payment: {type: Schema.Types.ObjectId, ref:'Payment', required: true },
+  delivery: {type: Schema.Types.ObjectId, ref:'Delivery', required: true },
+  canceled: { type: Boolean, default: false },
+  store:  {type: Schema.Types.ObjectId, ref:'Store', required: true  }, 
 
 },{ timestamps: true });
 
