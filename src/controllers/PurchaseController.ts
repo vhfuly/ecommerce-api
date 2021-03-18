f3791b2439189a8e81f193e6af97557f47433390
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-18 19:30:24 -0300

    feat: create PurchaseController client

--- src/controllers/PurchaseController.ts
+++ src/controllers/PurchaseController.ts
@@ -7,6 +7,7 @@ import Product from '@models/Product';
 import Delivery from '@models/Delivery';
 import Payment from '@models/Payment';
 import Variation from '@models/Variation';
+import { json } from 'body-parser';
 
 class PurchaseController {
   //ADMIN
@@ -63,6 +64,120 @@ class PurchaseController {
       next(error);
     }
   }
+  //CLIENT
+  async index(request: Request, response: Response , next: NextFunction) {
+    const { store } = request.query;
+    const offset = Number(request.query.offset) || 0;
+    const limit = Number(request.query.limit) || 0;
+    try {
+      const client = await Client.findOne({user: request.payload.id});
+      if (!client) return response.status(400).json({Error: 'Client not found'});
+      const purchases = await Purchase.find({store: String(store), client: client._id })
+        .skip(offset).limit(limit)
+        .populate(['client', 'payment', 'delivery', 'variation', 'product']);
+      return response.json({ purchases, offset, limit, total: purchases.length });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async show(request: Request, response: Response , next: NextFunction) {
+    const { id } = request.params;
+    try {
+      const client = await Client.findOne({user: request.payload.id});
+      if (!client) return response.status(400).json({Error: 'Client not found'});
+      const purchase = await Purchase.findOne({ client: client._id, _id: id })
+        .populate(['client', 'payment', 'delivery', 'variation', 'product']);
+      return response.json(purchase);
+    } catch (error) {
+      next(error);
+    }
+  }
+  
+
+  async store(request: Request, response: Response , next: NextFunction) {
+    const { cart, payment, delivery} = request.body;
+    const { store } = request.query;
+    try {
+      // if(!await CartValidation(cart)) return response.status(422).json({ error: 'Invalid cart' });
+
+      // if(!await DeliveryValidation(cart, delivery)) return response.status(422).json({ error: 'Invalid data delivery' });
+
+      // if(!await PaymentValidation(cart, payment)) return response.status(422).json({ error: 'Invalid data payment' });
+
+      const client = await Client.findOne({user: request.payload.id});
+
+      const newPayment = new Payment({
+        value: payment.value,
+        type: payment.type,
+        status: 'initiated',
+        payload: payment,
+        store,
+      });
+
+      const newDelivery = new Delivery({
+        status: 'not_started',
+        cost: delivery.cost,
+        deadline: delivery.deadline,
+        payload: delivery,
+        store,
+      });
+
+      const purchase = new Purchase({
+        client: client._id,
+        cart,
+        payment: newPayment._id,
+        delivery: newDelivery._id,
+        store,
+      });
+      newDelivery.purchase = purchase._id;
+      newPayment.purchase = purchase._id;
+
+      await purchase.save();
+      await newDelivery.save();
+      await newPayment.save();
+
+      //notificar via e-mail - client e admin
+
+      response.json({
+        purchase: Object.assign({}, purchase, { delivery: newDelivery, payment: newPayment, client: client })
+      });
+    } catch (error) {
+      
+    }
+  }
+  
+  async remove(request: Request, response: Response , next: NextFunction) {
+    const { store } = request.query;
+    const { id } = request.params;
+    try {
+      const client = await Client.findOne({user: request.payload.id});
+      if (!client) return response.status(400).json({Error: 'Client not found'});
+      const purchase = await Purchase.findOne({ client: client._id, _id: id })
+      if (!purchase) return response.status(400).json({Error: 'Purchase not found'});
+      purchase.canceled = true;
+
+      //registro de atividade = pedido cancelado
+      // enviar email para admin 
+
+      return response.json({ canceled: true });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async showCartPurchase(request: Request, response: Response , next: NextFunction) {
+    const { id } = request.params;
+    try {
+      const client = await Client.findOne({user: request.payload.id});
+      if (!client) return response.status(400).json({Error: 'Client not found'});
+      const purchase = await Purchase.findOne({ client: client._id, _id: id })
+      return response.json({ cart: purchase.cart });
+    } catch (error) {
+      next(error);
+    }
+  }
+
 }
 
 export { PurchaseController }
