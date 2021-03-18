bfd6777854359f72e231ddfd39df4cda99d44d5f
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-18 19:37:15 -0300

    feat: create DeliveryModel

---
+++ src/models/Delivery.ts
@@ -0,0 +1,31 @@
+import { Document, Schema, model, Model } from 'mongoose'
+import { ClientInterface } from '@interfaces/Client';
+
+export interface DeliveryDocument extends DeliveryInterface, Document {}
+
+const DeliverySchema: Schema<DeliveryDocument, DeliveryModel> = new Schema({
+  status: { type: String, required: true },
+  trackingCode: { type: String },
+  type: { type: String, required: true },
+  cost: { type: Number, required: true },
+  deadline: { type: Number, required: true },
+  address: {
+      type: {
+          place: { type: String, required: true },
+          number: { type: String, required: true },
+          complement: { type: String },
+          district: { type: String, required: true },
+          city: { type: String, required: true },
+          state: { type: String, required: true },
+          ZipCode: { type: String, required: true }
+      },
+      required: true
+  },
+  purchase: { type: Schema.Types.ObjectId, ref: "Purchase", required: true },
+  store: { type: Schema.Types.ObjectId, ref: "store", required: true },
+  payload: { type: Object }
+},{ timestamps: true });
+
+
+export interface DeliveryModel extends Model<DeliveryDocument> {}
+export default model<DeliveryDocument, DeliveryModel>('Delivery', DeliverySchema)
