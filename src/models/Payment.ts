c87e889dc38792181fb29a344e6a682456793997
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-18 19:41:53 -0300

    feat: create PaymentModel

---
+++ src/models/Payment.ts
@@ -0,0 +1,18 @@
+import { Document, Schema, model, Model } from 'mongoose'
+
+export interface PaymentDocument extends PaymentInterface, Document {}
+
+const PaymentSchema: Schema<PaymentDocument, PaymentModel> = new Schema({
+  status: { type: String, required: true },
+  trackingCode: { type: String },
+  type: { type: String, required: true },
+  cost: { type: Number, required: true },
+  deadline: { type: Number, required: true },
+  purchase: { type: Schema.Types.ObjectId, ref: "Purchase", required: true },
+  store: { type: Schema.Types.ObjectId, ref: "store", required: true },
+  payload: { type: Object }
+},{ timestamps: true });
+
+
+export interface PaymentModel extends Model<PaymentDocument> {}
+export default model<PaymentDocument, PaymentModel>('Payment', PaymentSchema)
