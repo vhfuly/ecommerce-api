2274be87e70626617c19dceebd7404236407201a
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-20 09:00:26 -0300

    feat: create purchaseValidation

---
+++ src/controllers/validations/PurchaseValidation.ts
@@ -0,0 +1,79 @@
+import Joi from 'joi';
+
+const purchaseValidation = { 
+  indexAdmin: {
+    query: Joi.object().keys({
+      limit: Joi.number(),
+      offset: Joi.number(),
+    }),
+  },
+
+  showAdmin: {
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24).required(),
+    }),
+  },
+
+  removeAdmin: {
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24).required(),
+    }),
+  },
+
+  showCartPurchaseAdmin: {
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24).required(),
+    }),
+  },
+
+  index: {
+    query: Joi.object().keys({
+      store: Joi.string().alphanum().length(24).required(),
+      limit: Joi.number(),
+      offset: Joi.number(),
+    }),
+  },
+
+  show: {
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24).required(),
+    }),
+  },
+
+  store: {
+    query: Joi.object().keys({
+      store: Joi.string().alphanum().length(24).required(),
+    }),
+
+    body: Joi.object().keys({
+      cart: Joi.array().items(Joi.object({
+        variation: Joi.string().alphanum().length(24).required(),
+        product: Joi.string().alphanum().length(24).required(),
+        amount:  Joi.number().required(),
+        unitPrice:  Joi.number().required(),
+      })).required(),
+      payment: Joi.object({
+        value:  Joi.number().required(),
+        type:  Joi.string().required(),
+      }).required(),
+      delivery: Joi.object({
+        cost: Joi.number().required(),
+        deadline: Joi.number().required(),
+      }).required(),
+    }),
+  },
+
+  remove: {
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24).required(),
+    }),
+  },
+
+  showCartPurchase: {
+    params: Joi.object().keys({
+      id: Joi.string().alphanum().length(24).required(),
+    }),
+  },
+}
+
+export { purchaseValidation }
--- src/routes/api/v1/Purchases.ts
+++ src/routes/api/v1/Purchases.ts
@@ -3,6 +3,7 @@ import { validate } from 'express-validation';
 
 import { auth } from '../../auth';
 import { storeValidation } from '@validations/StoreValidation';
+import { purchaseValidation } from '@validations/PurchaseValidation';
 import { PurchaseController } from '@controllers/PurchaseController';
 const purchaseController = new PurchaseController();
 
@@ -10,23 +11,23 @@ const router = Router();
 
 
 // ADMIN
-router.get("/admin", auth.required, storeValidation.admin, purchaseController.indexAdmin);
-router.get("/admin/:id", auth.required, storeValidation.admin, purchaseController.showAdmin);
+router.get("/admin", auth.required, storeValidation.admin, validate(purchaseValidation.indexAdmin), purchaseController.indexAdmin);
+router.get("/admin/:id", auth.required, storeValidation.admin, validate(purchaseValidation.showAdmin), purchaseController.showAdmin);
 
-router.delete("/admin/:id", auth.required, storeValidation.admin, purchaseController.removeAdmin);
+router.delete("/admin/:id", auth.required, storeValidation.admin, validate(purchaseValidation.removeAdmin), purchaseController.removeAdmin);
 
 // ADMIN -- cart
-router.get("/admin/:id/cart", auth.required, storeValidation.admin, purchaseController.showCartPurchaseAdmin);
+router.get("/admin/:id/cart", auth.required, storeValidation.admin, validate(purchaseValidation.showCartPurchaseAdmin), purchaseController.showCartPurchaseAdmin);
 
 // CLIENT
-router.get("/", auth.required, purchaseController.index);
-router.get("/:id", auth.required, purchaseController.show);
+router.get("/", auth.required, validate(purchaseValidation.index), purchaseController.index);
+router.get("/:id", auth.required, validate(purchaseValidation.show), purchaseController.show);
 
-router.post("/", auth.required, purchaseController.store);
-router.delete("/:id", auth.required, purchaseController.remove);
+router.post("/", auth.required, validate(purchaseValidation.store), purchaseController.store);
+router.delete("/:id", auth.required, validate(purchaseValidation.remove), purchaseController.remove);
 
 //  CLIENT -- cart
-router.get("/:id/cart", purchaseController.showCartPurchase);
+router.get("/:id/cart", validate(purchaseValidation.showCartPurchase), purchaseController.showCartPurchase);
 
 
 export { router as purchasesRouter }
