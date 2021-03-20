fb206075898964aa4c565643732be5dc85eaee3e
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-20 10:44:07 -0300

    feat: create PurchasesRecordDocument

---
+++ src/models/PurchasesRecord.ts
@@ -0,0 +1,15 @@
+import { Document, Schema, model, Model } from 'mongoose';
+import { PurchaseInterface } from '@interfaces/Purchase';
+
+export interface PurchasesRecordDocument extends PurchasesRecordInterface, Document {}
+
+const PurchasesRecordSchema: Schema<PurchasesRecordDocument, PurchasesRecordModel> = new Schema({
+  purchase: { type: Schema.Types.ObjectId, ref:'Purchase', required: true },
+  type: { type: String, required: true },
+  status: { type: String, required: true },
+  date: { type: Date, default: Date.now() },
+  payload: { type: Object },
+},{ timestamps: true });
+
+export interface PurchasesRecordModel extends Model<PurchasesRecordDocument> {}
+export default model<PurchasesRecordDocument, PurchasesRecordModel>('PurchasesRecord', PurchasesRecordSchema);
