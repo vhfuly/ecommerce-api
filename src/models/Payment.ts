308217f75fe16374d98a56cff77f04a0fccb8895
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-21 09:17:30 -0300

    feat: update PaymentSchema

--- src/models/Payment.ts
+++ src/models/Payment.ts
@@ -6,10 +6,34 @@ export interface PaymentDocument extends PaymentInterface, Document {}
 const PaymentSchema: Schema<PaymentDocument, PaymentModel> = new Schema({
   value: { type: Number, required: true },
   type: { type: String, required: true },
-  parceled: { type: Object },
+  parcel: { type: Number, dafault: 1 },
+  address: {
+    type: {
+        street: { type: String, required: true },
+        number: { type: String, required: true },
+        complement: { type: String },
+        district: { type: String, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        ZipCode: { type: String, required: true }
+    },
+    required: true
+  },
+  card: {
+    type: {
+      name: { type: String, required: true },
+      areCode: { type: String, required: true },
+      phone: { type: String, required: true},
+      birthDate: { type: String, required: true },
+      creditCardToken: { type: String, required: true },
+      cpf: { type: String, required: true },
+    },
+  },
+  sameBillingAddress: { type: Boolean, default: true},
   purchase: { type: Schema.Types.ObjectId, ref: "Purchase", required: true },
   store: { type: Schema.Types.ObjectId, ref: "store", required: true },
-  payload: { type: Object }
+  payload: { type: Array },
+  pagseguroCode: { type: String },
 },{ timestamps: true });
 
 
