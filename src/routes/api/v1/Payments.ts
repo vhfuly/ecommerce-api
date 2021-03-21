e9a4473c58ec8ab225cada58e8e3ba9ef2a85a1c
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-21 09:51:30 -0300

    feat: create paymentsRouter

---
+++ src/routes/api/v1/Payments.ts
@@ -0,0 +1,28 @@
+import { Router, Request, Response } from 'express';
+import { validate } from 'express-validation';
+
+
+import { auth } from '../../auth';
+import { storeValidation } from '@validations/StoreValidation';
+import { PaymentController } from '@controllers/PaymentController';
+const paymentController = new PaymentController();
+const router = Router();
+
+//teste tokens
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/tokens', (request: Request, response: Response) => response.render('pagseguro/index'));
+}
+
+//pagsegro
+router.post('/notification', paymentController.notification);
+router.get('/session', paymentController.getSessionId);
+
+//client
+router.get('/:id', auth.required, paymentController.show);
+router.post('/pay/:id', auth.required, paymentController.pay);
+
+//ADMIN
+
+router.put('/:id', auth.required, storeValidation.admin, paymentController.update);
+
+export { router as paymentsRouter }
--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -8,6 +8,7 @@ import { assessmentsRouter } from './Assessments';
 import { variationsRouter } from './Variations';
 import { purchasesRouter } from './Purchases';
 import { deliveriesRouter } from './Deliveries';
+import { paymentsRouter } from './Payments';
 
 const router = Router();
 
@@ -20,5 +21,6 @@ router.use('/assessments', assessmentsRouter)
 router.use('/variations', variationsRouter)
 router.use('/purchases', purchasesRouter)
 router.use('/deliveries', deliveriesRouter)
+router.use('/payments', paymentsRouter)
 
 export { router as routerApi }
