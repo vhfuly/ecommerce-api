fcb367fc57b795ca105d821ffb210af1ffcda092
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-21 08:13:31 -0300

    feat: create config pagseguro

---
+++ src/config/pageseguro.ts
@@ -0,0 +1,8 @@
+export default {
+  mode: process.env.NODE_ENV === 'production' ? 'live' : 'sandbox',
+  sandbox: process.env.NODE_ENV === 'production' ? false : true,
+  sandbox_email: process.env.NODE_ENV === 'production' ? null : 'email@sandbox.pagseguro.com.br',
+  email: process.env.EMAIL_PAGSEGURO,
+  token: process.env.TOKEN_PAGSEGURO,
+  notificationURL: ''//url de notificação ,
+}
